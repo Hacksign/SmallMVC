81fdae7354f316502f7bf0d777ca5b805e072e0f
Hacksign <evilsign@gmail.com>
2013-05-17 00:01:20 +0800

    fix where xxx like bug

--- plugins/database/SmallMVCDriverMySQL.php
+++ plugins/database/SmallMVCDriverMySQL.php
@@ -101,7 +101,7 @@ class SmallMVCDriverMySQL{
 		if(is_string($clause))
 			$clause = array($clause);
 		foreach($clause as &$each){
-			if(preg_match('/\w*\bis\b(\s*(not)){0,1}/',$each))
+			if(preg_match('/(\w*\bis\b(\s*(not)){0,1})|(\w*\blike\b)/',$each))
 				$each = ' '.$each.' ';
 			else if(!preg_match('/[=<>]/',$each))
 			 $each .= ' = ';  
