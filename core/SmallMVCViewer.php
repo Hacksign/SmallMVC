3c420bbcb2c3db7fc573924c5a11ae786356098d
Hacksign <evilsign@gmail.com>
2014-11-11 18:35:35 +0800

    change template quote character to {{}}

--- core/SmallMVCViewer.php
+++ core/SmallMVCViewer.php
@@ -84,8 +84,7 @@ class SmallMVCViewer {
 			$matches = null;
 			foreach($lines as $line){
 				$line = trim($line);
-				//olny match single line which is quoted by '{}'
-				$num = preg_match_all('/{([^{}]+?)}/', $line, $matches);
+				$num = preg_match_all('/\{\{([^{}]+?)\}\}/', $line, $matches);
 				for($i = 0; $i < $num; $i++) {
 					$match = $matches[0][$i];
 					$new = $this->transformSyntax($matches[1][$i]);
