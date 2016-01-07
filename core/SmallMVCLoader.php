fa683e795892d85dcb8156831d29fc9060728042
shipengfei <shipengfei@360.cn>
2016-01-07 14:43:32 +0800

    update

--- core/SmallMVCLoader.php
+++ core/SmallMVCLoader.php
@@ -195,20 +195,24 @@ class SmallMVCLoader{
             $includePath = implode(PS, SMvc::instance(null, 'default')->config['project']['directory']);
             $includePath = str_replace(DS.DS, DS, $includePath);
         }else{
-            //pick out path not allowed by php settings'open_basedir'
-            // . is a exception
-            $allowedPath = explode(PS, ini_get('open_basedir'));
-            $includePath = explode(PS, get_include_path());
-            foreach($includePath as $eachPath){
-                if($eachPath !== '.'){
-                    if(in_array($eachPath, $allowedPath)){
+            if(!empty(ini_get('open_basedir'))){
+                //pick out path not allowed by php settings'open_basedir'
+                // . is a exception
+                $allowedPath = explode(PS, ini_get('open_basedir'));
+                $includePath = explode(PS, get_include_path());
+                foreach($includePath as $eachPath){
+                    if($eachPath !== '.'){
+                        if(in_array($eachPath, $allowedPath)){
+                            $checkedPath[] = $eachPath;
+                        }
+                    }else{
                         $checkedPath[] = $eachPath;
                     }
-                }else{
-                    $checkedPath[] = $eachPath;
                 }
+                $includePath = implode(PS, SMvc::instance(null, 'default')->config['project']['directory']) . PS . implode(PS, $checkedPath);
+            }else{
+                $includePath = implode(PS, SMvc::instance(null, 'default')->config['project']['directory']) . PS . get_include_path();
             }
-            $includePath = implode(PS, SMvc::instance(null, 'default')->config['project']['directory']) . PS . implode(PS, $checkedPath);
         }
         $subPath = explode('.', $fileName);
         $fileName = implode('.', array_slice($subPath, -2, 2));
